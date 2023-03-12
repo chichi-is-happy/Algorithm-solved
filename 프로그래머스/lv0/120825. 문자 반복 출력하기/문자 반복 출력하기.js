4b9ed4c852800d7ac2036b1c75b9962d572f20d4
chichi <107830853+chichi-is-happy@users.noreply.github.com>
2023-03-12 20:26:19 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 3.60 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,8 @@
+const solution = (my_string, n) => {
+  let result = "";
+  for(let i = 0; i < my_string.length; i++) {
+    result = result + my_string[i].repeat(n);
+    console.log(result)
+  }
+  return result;
+}
