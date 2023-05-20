21d234348d4daedbd199382244c2934cbc85dc57
chichi <107830853+chichi-is-happy@users.noreply.github.com>
2023-05-20 23:07:46 +0900

    [level 0] Title: 모스부호 (1), Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,19 @@
+const solution = letter => {
+  const morse = {
+    '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+    '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+    '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+    '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+    '-.--': 'y', '--..': 'z'
+  };
+
+  const words = letter.split(' ');
+  let result = '';
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    result += morse[word];
+  }
+
+  return result;
+}
